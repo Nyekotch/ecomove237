56ad7255aa9ce1ba8733effd21e55bd4823e9634
MACBOOK 13 2020 <pagaljunior@ANGOs-MacBook-Pro.local>
2025-11-03 10:29:51 +0100

    bonne modif

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.jpg";
+import logo from "@/assets/images/logo.jpg";
 
 export default function Footer() {
   const { t } = useTranslation();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg'],
+  build: {
+    assetsInlineLimit: 4096, // 4kb
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  }
 })
